4ffe62ae0fdee0bb60f4a28498eac7dde20cbecf
Noobwastaken99 <czmoni99@yahoo.com>
2023-03-12 18:54:33 -0500

    examples for anguar part 3 finished

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   goldInactive: boolean = false;
+   silverInactive: boolean = false;
+   copperInactive: boolean = false;
+
+   activateButtons () {
+      this.goldInactive = false;
+      this.silverInactive = false;
+      this.copperInactive = false;
+   }
 
    constructor() { }
 
